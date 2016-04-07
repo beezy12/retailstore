7a82cf8af3174cc6e9437ed70160d5ce4983aa64
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-04-07 16:24:21 -0500

    can insert a document and query it back. making a c1v6 branch

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
+'use strict'
+
+// const express = require('express')
+// const app = express()
+
 const _ = require('underscore')
-const mongod = require('mongod')
+const mongodb = require('mongodb')
 
-// type   node index.js  at the cl to see output
-// _.each([1, 2, 3], function(v) {
-//     console.log(v)
-// })
-
-
-/* Query for a document is slightly trickier.
-If you pass the call back to the driver's find function,
-you'll get back a cursor rather than a set of documents.
-In the context of MongoDB, a cursor
-is an object that you could call next on
-to get the next document.
-However, in order to make things easier
-for users who don't need fine grained control
-over the cursor, the driver has this nice chainable toArray
-function that exhausts the cursor for you
-and returns an array of documents in the call back.
-And once you run this program, you
-should see that MongoDB successfully
-inserted a document, and then query to back. */
 
 const uri = 'mongodb://localhost:27017/example'
 
+
+
+// here I am inserting a document a querying it back
 mongodb.MongoClient.connect(uri, function(error, db) {
     if(error) {
         console.log(error)
@@ -50,3 +38,13 @@ mongodb.MongoClient.connect(uri, function(error, db) {
         })
     })
 })
+
+
+
+// mongoose.connect(MONGODB_URL, (err) => {
+//     if(err) throw err
+//
+//     app.listen(PORT, () => {
+//         console.log(`server is running on port: ${PORT}`)
+//     })
+// })
